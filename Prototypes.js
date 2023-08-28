2172ee0cbb5a7e3615a933249614fb4ec146a2f2
Oloishorua <jacoboloishorua@gmail.com>
2023-08-29 01:27:04 +0300

    Using constructors and prototypes to make objects

--- Prototypes.js
+++ Prototypes.js
@@ -15,5 +15,17 @@ const book1 = new Book('Swords and Borns', 'Daniel Steel', 2012);
 
 const book2 = new Book('Pilgrim\' Progress', 'John Bunyan', 1996);
 
-console.log(book1.getDetails());
-console.log(book2)
+// console.log(book1.getDetails());
+// console.log(book2)
+
+
+function Magazine(title, author, year, month){
+    Book.call(this, title, author, year);
+
+    this.month = month
+}
+
+//magazine object
+
+const mag1 = new Magazine("Super Strikers", "Mdoe Salim", 2016, "January");
+console.log(mag1)
